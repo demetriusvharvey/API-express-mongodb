fe8b78aca588c6d585cbc0d3191663305cbf409b
demetriusvharvey <78282284+demetriusvharvey@users.noreply.github.com>
2021-04-12 01:33:18 -0400

    works

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require('./connection')
 let app = require('./app')
 
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("app listening on port 3000");
+// });
 
 app.set("port", process.env.PORT || 3000)
 let server = app.listen(app.get("port"))
